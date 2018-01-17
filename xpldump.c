f9234666f171656a409f8d194265d54202f00595
xdee <mail@x-dee.ru>
2018-01-17 14:52:29 +0500

    Reindent lines

--- xpldump.c
+++ xpldump.c
@@ -18,76 +18,76 @@ typedef struct Block
 } Block;
 
 int checkPrologue(char *data, char *data2) {
-  int i=0;
+	int i=0;
 
-  while(data[i]) {
-    if (data[i] != data2[i]) return 0;
-    i++;
-  }
-  return 1;
+	while(data[i]) {
+		if (data[i] != data2[i]) return 0;
+		i++;
+	}
+	return 1;
 }
 
 void dump(unsigned char* buf, int len) {
-  while(len--) {
-    printf("%x ", *buf);
-    buf++;
-  }
-  printf("\n");
+	while(len--) {
+		printf("%x ", *buf);
+		buf++;
+	}
+	printf("\n");
 }
 
 main(int argc, char *argv[]) {
-  char buf[1024];
-  
-  int sock;
-  struct sockaddr_in name;
-  struct hostent *hp, *gethostbyname();
-  int bytes;
+	char buf[1024];
 
-  printf("Listen activating.\n");
+	int sock;
+	struct sockaddr_in name;
+	struct hostent *hp, *gethostbyname();
+	int bytes;
+
+	printf("Listen activating.\n");
 
   /* Create socket from which to read */
-  sock = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sock < 0)   {
-    perror("Opening datagram socket");
-    exit(1);
-  }
-  
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0)   {
+		perror("Opening datagram socket");
+		exit(1);
+	}
+
   /* Bind our local address so that the client can send to us */
-  bzero((char *) &name, sizeof(name));
-  name.sin_family = AF_INET;
-  name.sin_addr.s_addr = htonl(INADDR_ANY);
-  name.sin_port = htons(SERVER_PORT);
-  
-  if (bind(sock, (struct sockaddr *) &name, sizeof(name))) {
-    perror("binding datagram socket");
-    exit(1);
-  }
-  
-  printf("Socket has port number #%d\n", ntohs(name.sin_port));
-  int i=0;
-  while ((bytes = read(sock, buf, 1024)) > 0) {
-    
-    printf("\e[1;1H\e[2J"); //clear screen and move coursor to 1:1
-    printf("#%d. recv bytes: %d\n",i++ , bytes);
-    dump(buf, bytes);
+	bzero((char *) &name, sizeof(name));
+	name.sin_family = AF_INET;
+	name.sin_addr.s_addr = htonl(INADDR_ANY);
+	name.sin_port = htons(SERVER_PORT);
 
-    if (bytes >= (PROL_SIZE + sizeof(Block)) && checkPrologue(PROL_DATA, buf)) {
-    	
-    	Block *msg = (Block*) (buf+PROL_SIZE);
-    	bytes -= PROL_SIZE;
+	if (bind(sock, (struct sockaddr *) &name, sizeof(name))) {
+		perror("binding datagram socket");
+		exit(1);
+	}
 
-    	while(bytes >= sizeof(Block)) {
-        printf("Index: %d\n", msg->index);
-        
-        for (int i=0; i<8; i++)
-          printf("\t%.3f", msg->data[i]);
-        printf("\n");
+	printf("Socket has port number #%d\n", ntohs(name.sin_port));
+	int i=0;
+	while ((bytes = read(sock, buf, 1024)) > 0) {
 
-    		bytes -= sizeof(Block);
-    		msg++;
-   		}
-    }
-  }
+		printf("\e[1;1H\e[2J"); //clear screen and move coursor to 1:1
+		printf("#%d. recv bytes: %d\n",i++ , bytes);
+		dump(buf, bytes);
 
-  close(sock);
+		if (bytes >= (PROL_SIZE + sizeof(Block)) && checkPrologue(PROL_DATA, buf)) {
+
+			Block *msg = (Block*) (buf+PROL_SIZE);
+			bytes -= PROL_SIZE;
+
+			while(bytes >= sizeof(Block)) {
+				printf("Index: %d\n", msg->index);
+
+				for (int i=0; i<8; i++)
+				printf("\t%.3f", msg->data[i]);
+				printf("\n");
+
+				bytes -= sizeof(Block);
+				msg++;
+			}
+		}
+	}
+
+	close(sock);
 }
