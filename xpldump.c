c6cefb7c978a261a8fd0bdfe2a123dd53ba6af4b
xdee <mail@x-dee.ru>
2018-01-19 16:07:49 +0500

    fix indentation

--- xpldump.c
+++ xpldump.c
@@ -33,16 +33,16 @@ int checkPrologue(char *data, char *data2) {
 }
 
 char* readableSize(double size, char *buf) {
-  int i = 0;
+	int i = 0;
 
-  const char* units[] = { "bytes", "KB", "MB", "GB", "TB", "PB" };
-  while (size>1024 && i < sizeof(units)) {
-    size /= 1024;
-    i++;
-  }
+	const char* units[] = { "bytes", "KB", "MB", "GB", "TB", "PB" };
+	while (size>1024 && i < sizeof(units)) {
+		size /= 1024;
+		i++;
+	}
 
-  sprintf(buf, "%.*f %s", i, size, units[i]);
-  return buf;
+	sprintf(buf, "%.*f %s", i, size, units[i]);
+	return buf;
 }
 
 void dump(unsigned char* buf, int len) {
@@ -54,27 +54,27 @@ void dump(unsigned char* buf, int len) {
 } 
 
 void intHandler(int sig) {
-  running = 0;
+	running = 0;
 }
 
 int main(int argc, char *argv[]) {
 
-  char buf[1024];
-  double total = 0;
-  int sock;
+	char buf[1024];
+	double total = 0;
+	int sock;
 	struct sockaddr_in name;
 	struct hostent *hp, *gethostbyname();
 	int bytes;
-  int doDump = 0;
+	int doDump = 0;
 
-  while(argc--) {
-    if (strcmp("-d", argv[argc]) == 0)
-      doDump = 1;
-  }
+	while(argc--) {
+		if (strcmp("-d", argv[argc]) == 0)
+			doDump = 1;
+	}
 
-  signal(SIGINT, intHandler);
+	signal(SIGINT, intHandler);
 
-  printf("Listen activating.\n");
+	printf("Listen activating.\n");
 
   /* Create socket from which to read */
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -85,7 +85,7 @@ int main(int argc, char *argv[]) {
 
   /* Bind our local address so that the client can send to us */
 	memset(&name, 0, sizeof(name));
-  name.sin_family = AF_INET;
+	name.sin_family = AF_INET;
 	name.sin_addr.s_addr = htonl(INADDR_ANY);
 	name.sin_port = htons(SERVER_PORT);
 
@@ -101,11 +101,11 @@ int main(int argc, char *argv[]) {
 		printf("\e[1;1H\e[2J"); //clear screen and move coursor to 1:1
 		printf("#%d. recv bytes: %d\n",i++ , bytes);
 		if (doDump)
-      dump(buf, bytes);
+			dump(buf, bytes);
 
 		total += bytes;
 
-    if (bytes >= (PROL_SIZE + sizeof(Block)) && checkPrologue(PROL_DATA, buf)) {
+		if (bytes >= (PROL_SIZE + sizeof(Block)) && checkPrologue(PROL_DATA, buf)) {
 
 			Block *msg = (Block*) (buf+PROL_SIZE);
 			bytes -= PROL_SIZE;
@@ -114,7 +114,7 @@ int main(int argc, char *argv[]) {
 				printf("Index: %d\n", msg->index);
 
 				for (int i=0; i<8; i++)
-				printf("\t%.3f", msg->data[i]);
+					printf("\t%.3f", msg->data[i]);
 				printf("\n");
 
 				bytes -= sizeof(Block);
@@ -123,6 +123,6 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-  close(sock);
-  printf("Total recv: %s\n", readableSize(total, buf));
+	close(sock);
+	printf("Total recv: %s\n", readableSize(total, buf));
 }
